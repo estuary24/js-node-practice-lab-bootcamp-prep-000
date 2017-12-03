cdd4ffe4f17d6b9f84ba55f7489184931d95b40c
Elizaveta Limanova <estuary24@gmail.com>
2017-12-03 15:35:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
-// write your code below!
+Name:
+Joe
+Height:
+74 
 
-var name = "Joe"
+var name = "joe"
 var height = "74"
 
-console.log("Name:")
+console.log("Name")
 console.log(name)
 
 console.log("Height:")
 console.log(height)
+
+nodejs index.js
+
+
